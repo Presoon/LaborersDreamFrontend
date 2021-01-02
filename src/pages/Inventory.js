ba65ce28fb341f3559af780177222329abd60638
Kamil <kamilszpak99@gmail.com>
2021-01-02 16:06:02 +0100

    Some clearing changes

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,50 +1,15 @@
 import React from "react";
-import UserService from "../services/user.service";
 
 class Inventory extends React.Component {
   state = {
     inventory: "",
   };
 
-  componentDidMount() {
-    UserService.getQRCodeInfo().then(
-      (response) => {
-        this.setState({
-          inventory: response.data,
-        });
-      },
-      (error) => {
-        this.setState({
-          inventory:
-            (error.response && error.response.data) ||
-            error.message ||
-            error.toString(),
-        });
-      }
-    );
-  }
-
   render() {
     const { inventory } = this.state;
     return (
       <>
         <h1>Inventory Page</h1>
-        {
-          <div>
-            <p>
-              <span>ID: </span>
-              <b>{inventory.id}</b>
-            </p>
-            <p>
-              <span>Name: </span>
-              <b>{inventory.specification}</b>
-            </p>
-            <p>
-              <span>Series Number: </span>
-              <b>{inventory.seriesNumber}</b>
-            </p>
-          </div>
-        }
       </>
     );
   }
