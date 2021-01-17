f31a0f42e71d7ad43e9147c59766e66ef6749e15
Pawcio <paulanersilva@gmail.com>
2021-01-17 18:11:37 +0100

    Add home page fix for mobile devices

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,48 +36,48 @@ class Home extends React.Component {
           <h1 className="ml-auto">{this.state.time}</h1>
         </div>
         <div className="container">
-          <div className="row mt-5">
-            <div className="col text-center">
+          <div className="row">
+            <div className="col-8 col-md-4 text-center mt-4">
               <NavLink className="nav-link d-flex flex-column border border-primary rounded" to="/newticket">
-                <i className="fas fa-bug fa-7x"/>
+                <i className="fas fa-bug fa-6x"/>
                 <span className="mt-3 display-4">Utwórz zgłoszenie</span>
               </NavLink>
             </div>
-            <div className="col text-center">
+            <div className="col-8 col-md-4 text-center mt-4">
             <NavLink className="nav-link d-flex flex-column border border-primary rounded" to="/tickets">
-                      <i className="fas fa-book fa-7x"/>
+                      <i className="fas fa-book fa-6x"/>
                       <span className="mt-3 display-4">System zgłoszeń</span>
                     </NavLink>
             </div>
-            <div className="col text-center">
+            <div className="col-8 col-md-4 text-center mt-4">
             <NavLink className="nav-link d-flex flex-column border border-primary rounded" to="/inventory">
-                      <i className="fas fa-cubes fa-7x"/>
+                      <i className="fas fa-cubes fa-6x"/>
                       <span className="mt-3 display-4">Inwentarz</span>
                     </NavLink>
             </div>
           </div>
-          <div className="row mt-4">
-            <div className="col text-center">
+          <div className="row">
+            <div className="col-8 col-md-4 text-center mt-4">
             <NavLink className="nav-link d-flex flex-column border border-primary rounded" to="/check">
-                  <i className="fas fa-question-circle fa-7x" />
+                  <i className="fas fa-question-circle fa-6x" />
                   <span className="mt-3 display-4">Sprawdź sprzęt</span>
                 </NavLink>
             </div>
-            <div className="col text-center">
+            <div className="col-8 col-md-4 text-center mt-4">
             <NavLink className="nav-link d-flex flex-column border border-primary rounded" to="/users">
-                      <i className="fas fa-users fa-7x"/>
+                      <i className="fas fa-users fa-6x"/>
                       <span className="mt-3 display-4">Użytkownicy</span>
                     </NavLink>
             </div>
-            <div className="col text-center">
+            <div className="col-8 col-md-4 text-center mt-4">
             <NavLink className="nav-link d-flex flex-column border border-primary rounded" to="/localizations">
-                      <i className="fas fa-globe fa-7x"/>
+                      <i className="fas fa-globe fa-6x"/>
                       <span className="mt-3 display-4">Lokalizacje</span>
                     </NavLink>
             </div>
             </div>
-            <div className="row mt-4">
-              <div className="col text-center">
+            <div className="row">
+              <div className="col-8 col-md-4 text-center mt-4">
                 <NavLink className="nav-link d-flex flex-column border border-primary rounded"
                   to="/login"
                   onClick={() => {
@@ -85,13 +85,13 @@ class Home extends React.Component {
                     window.location.reload();
                   }}
                 >
-                  <i className="fas fa-user-circle fa-7x"/>
+                  <i className="fas fa-user-circle fa-6x"/>
                   <span className="mt-3 display-4">Wyloguj</span>
                 </NavLink>
               </div>
-              <div className="col text-center">
+              <div className="col-8 col-md-4 text-center mt-4">
               </div>
-              <div className="col text-center">
+              <div className="col-8 col-md-4 text-center mt-4">
               </div>
           </div>
         </div>
